2b2f77a6ef6f50c8911ec2ef953fe3210b83a1b0
Edd321 <36410503+Edd321@users.noreply.github.com>
2019-10-20 20:12:28 +0200

    Add files via upload

---
+++ palindrome.java
@@ -0,0 +1,29 @@
+package Algorithms;
+
+
+public class palindrome {
+
+	public static boolean isPalindrome(String str) {
+		boolean check = false;
+		String[] array = new String[str.length()];
+		String[] secondArray = new String[str.length()];
+		array = str.split("");
+		int last = array.length - 1;
+		int count = 0;
+		
+		for (int i = 0; i < array.length; i++) {
+			secondArray[i] = array[last];
+			last -= 1;
+		}
+		for (int j = 0; j < array.length; j++) {
+			if (array[j].equals(secondArray[j])) {
+				count += 1;
+			}
+		}
+		if (count == array.length) {
+			check = true;
+		}
+		return check;
+	}
+
+}
